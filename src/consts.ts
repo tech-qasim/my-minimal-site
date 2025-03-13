e5cfbee63b239e88865d1f81a2505811ea087e75
Dnzzk2 <1933226149@qq.com>
2025-03-14 00:32:01 +0800

    feat : Add some og tags and consts

---
+++ src/consts.ts
@@ -0,0 +1,10 @@
+type Site = {
+  title: string;
+  description: string;
+};
+
+//  TODO: 需要修改
+export const SITE: Site = {
+  title: "Astro Dk2",
+  description: "Astro Dk2 is a blog theme built with Astro.js and Dnzzk2.",
+};
