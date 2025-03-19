a9fa3f397249132760aa8ad449e05d9349186cca
Dnzzk2 <1933226149@qq.com>
2025-03-20 00:19:13 +0800

    feat:  Add lucide-react dependency, update header components and styles, optimize theme switching functionality

--- src/consts.ts
+++ src/consts.ts
@@ -3,8 +3,23 @@ type Site = {
   description: string;
 };
 
-//  TODO: 需要修改
+type Link = {
+  name: string;
+  url: string;
+};
+
 export const SITE: Site = {
   title: "Astro Dk2",
   description: "Astro Dk2 is a blog theme built with Astro.js and Dnzzk2.",
 };
+
+export const LINKS: Link[] = [
+  {
+    name: "Posts",
+    url: "/",
+  },
+  {
+    name: "Works",
+    url: "/works",
+  },
+];
