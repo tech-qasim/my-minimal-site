e608f3682b5246bce484874d407ffdc6f0287e8f
Dnzzk2 <1933226149@qq.com>
2025-04-30 11:13:57 +0800

    fix(plugins):  Update favicon source URL to improve stability and recommend usage
    Change the favicon source URL from icon.horse to Google's favicon service to improve stability and recommend usage. Simultaneously updated the source URL of the GitHub icon to maintain consistency

--- plugins/index.ts
+++ plugins/index.ts
@@ -29,12 +29,15 @@ export const remarkPlugins = [
         },
       },
       link: {
-        faviconSourceUrl: 'https://icon.horse/icon/{domain}',
+        // 1000 / month
+        // faviconSourceUrl: 'https://icon.horse/icon/{domain}',
+        // recommended ✨
+        faviconSourceUrl: 'https://www.google.com/s2/favicons?domain={domain}&sz=128',
         imgProps: (node: Parameters<PropertiesFromTextDirective>[0]) => {
           const props: ReturnType<PropertiesFromTextDirective> = {
             'aria-hidden': 'true',
           }
-          if (node.attributes?.class?.includes('github')) props.src = 'https://icon.horse/icon/github.com'
+          if (node.attributes?.class?.includes('github')) props.src = 'https://www.google.com/s2/favicons?domain=github.com&sz=128'
 
           return props
         },
