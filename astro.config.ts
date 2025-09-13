500b300ea804fce14672679e0199f1460f89b468
Qasim <qasimzexal@gmail.com>
2025-09-13 18:55:08 +0500

    init commit

--- astro.config.ts
+++ astro.config.ts
@@ -8,20 +8,28 @@ import expressiveCode from 'astro-expressive-code'
 import { remarkPlugins, rehypePlugins } from './plugins'
 import { SITE } from './src/config'
 
+import netlify from '@astrojs/netlify'
+
 export default defineConfig({
   site: SITE.website,
   base: SITE.base,
+
   prefetch: {
     prefetchAll: true,
     defaultStrategy: 'viewport',
   },
+
   vite: {
+    // @ts-expect-error Vite plugin type mismatch (fixed in Astro 6 / Vite 7)
     plugins: [tailwindcss()],
   },
+
   markdown: {
     syntaxHighlight: false,
     remarkPlugins,
     rehypePlugins,
   },
+
   integrations: [expressiveCode(), mdx(), react(), sitemap(), robotsTxt()],
+  adapter: netlify(),
 })
